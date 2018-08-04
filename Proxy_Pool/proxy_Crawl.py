64ae08b3487dc178639ac5e633745de48599012c
tj19961211 <tj1211keynote@outlook.com>
2018-08-04 15:42:23 +0800

    代理的抓取检测与维护

---
+++ Proxy_Pool/proxy_Crawl.py
@@ -0,0 +1,121 @@
+import json
+import re
+from Proxy_Pool.utils import get_page
+from pyquery import PyQuery as pq
+
+
+class ProxyMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        count = 0
+        attrs['__CrawlFunc__'] = []
+        for k, v in attrs.items():
+            if 'crawl_' in k:
+                attrs['__CrawlFunc__'].append(k)
+                count += 1
+        attrs['__CrawlFuncCount__'] = count
+        return type.__new__(cls, name, bases, attrs)
+
+
+class Crawler(object, metaclass=ProxyMetaclass):
+    def get_proxies(self, callback):
+        proxies = []
+        for proxy in eval("self.{}()".format(callback)):
+            print('成功获取到代理', proxy)
+            proxies.append(proxy)
+        return proxies
+
+
+    def crawl_daili66(self, page_count=4):
+        """
+        获取代理66
+        :param page_count: 页码
+        :return: 代理
+        """
+        start_url = 'http://www.66ip.cn/{}.html'
+        urls = [start_url.format(page) for page in range(1, page_count + 1)]
+        for url in urls:
+            print('Crawling', url)
+            html = get_page(url)
+            if html:
+                doc = pq(html)
+                trs = doc('.containerbox table tr:gt(0)').items()
+                for tr in trs:
+                    ip = tr.find('td:nth-child(1)').text()
+                    port = tr.find('td:nth-child(2)').text()
+                    yield ':'.join([ip, port])
+
+
+
+    def crawl_ip181(self):
+        start_url = 'http://www.ip181.com/'
+        html = get_page(start_url)
+        ip_address = re.compile('<tr.*?>\s*<td>(.*?)</td>\s*<td>(.*?)</td>')
+        # \s* 匹配空格，起到换行作用
+        re_ip_address = ip_address.findall(html)
+        for address, port in re_ip_address:
+            result = address + ':' + port
+            yield result.replace(' ', '')
+
+    def crawl_ip3366(self):
+        for page in range(1, 4):
+            start_url = 'http://www.ip3366.net/free/?stype=1&page={}'.format(page)
+            html = get_page(start_url)
+            ip_address = re.compile('<tr>\s*<td>(.*?)</td>\s*<td>(.*?)</td>')
+            # \s * 匹配空格，起到换行作用
+            re_ip_address = ip_address.findall(html)
+            for address, port in re_ip_address:
+                result = address + ':' + port
+                yield result.replace(' ', '')
+
+
+
+
+    def crawl_kuaidaili(self):
+        for i in range(1, 4):
+            start_url = 'http://www.kuaidaili.com/free/inha/{}/'.format(i)
+            html = get_page(start_url)
+            if html:
+                ip_address = re.compile('<td data-title="IP">(.*?)</td>')
+                re_ip_address = ip_address.findall(html)
+                port = re.compile('<td data-title="PORT">(.*?)</td>')
+                re_port = port.findall(html)
+                for address, port in zip(re_ip_address, re_port):
+                    address_port = address + ':' + port
+                    yield address_port.replace(' ', '')
+
+
+
+    def crawl_ip3366(self):
+        for i in range(1, 4):
+            start_url = 'http://www.ip3366.net/?stype=1&page={}'.format(i)
+            html = get_page(start_url)
+            if html:
+                find_tr = re.compile('<tr>(.*?)</tr>', re.S)
+                trs = find_tr.findall(html)
+                for s in range(1, len(trs)):
+                    find_ip = re.compile('<td>(\d+\.\d+\.\d+\.\d+)</td>')
+                    re_ip_address = find_ip.findall(trs[s])
+                    find_port = re.compile('<td>(\d+)</td>')
+                    re_port = find_port.findall(trs[s])
+                    for address, port in zip(re_ip_address, re_port):
+                        address_port = address + ':' + port
+                        yield address_port.replace(' ', '')
+
+    def crawl_iphai(self):
+        start_url = 'http://www.iphai.com/free/ng'
+        html = get_page(start_url)
+        if html:
+            find_tr = re.compile('<tr>(.*?)</tr>', re.S)
+            trs = find_tr.findall(html)
+            for s in range(1, len(trs)):
+                find_ip = re.compile('<td>\s+(\d+\.\d+\.\d+\.\d+)\s+</td>', re.S)
+                re_ip_address = find_ip.findall(trs[s])
+                find_port = re.compile('<td>\s+(\d+)\s+</td>', re.S)
+                re_port = find_port.findall(trs[s])
+                for address, port in zip(re_ip_address, re_port):
+                    address_port = address + ':' + port
+                    yield address_port.replace(' ', '')
+
+
+
+
